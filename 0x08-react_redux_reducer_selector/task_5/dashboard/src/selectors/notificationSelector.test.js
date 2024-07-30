7de36558cd54f6ade81ea7c80530806451c73c25
mudauzwothe <mudauzwothe@gmail.com>
2024-07-30 21:06:41 +0000

    inam

---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/selectors/notificationSelector.test.js
@@ -0,0 +1,38 @@
+import { fromJS } from 'immutable';
+import { filterTypeSelected, getNotifications, getUnreadNotifications } from './notificationSelector';
+
+describe('notification selectors', () => {
+  const state = fromJS({
+    filter: 'URGENT',
+    notifications: {
+      1: { id: 1, isRead: false, type: 'default', value: 'New course available' },
+      2: { id: 2, isRead: true, type: 'urgent', value: 'New resume available' },
+      3: { id: 3, isRead: false, type: 'urgent', value: 'New data available' }
+    }
+  });
+
+  it('filterTypeSelected should return the filter type', () => {
+    const result = filterTypeSelected(state);
+    expect(result).toBe('URGENT');
+  });
+
+  it('getNotifications should return the list of notifications', () => {
+    const result = getNotifications(state).toJS();
+    const expected = {
+      1: { id: 1, isRead: false, type: 'default', value: 'New course available' },
+      2: { id: 2, isRead: true, type: 'urgent', value: 'New resume available' },
+      3: { id: 3, isRead: false, type: 'urgent', value: 'New data available' }
+    };
+    expect(result).toEqual(expected);
+  });
+
+  it('getUnreadNotifications should return the list of unread notifications', () => {
+    const result = getUnreadNotifications(state).toJS();
+    const expected = {
+      1: { id: 1, isRead: false, type: 'default', value: 'New course available' },
+      3: { id: 3, isRead: false, type: 'urgent', value: 'New data available' }
+    };
+    expect(result).toEqual(expected);
+  });
+});
+
